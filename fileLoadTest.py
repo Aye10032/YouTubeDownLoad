80e0102bd5f9faaa4a5e47fb0cbe1f1e32fbb930
Dazo66 <895981998@qq.com>
2019-03-24 02:15:38 +0800

    阿叶看一哈

--- fileLoadTest.py
+++ fileLoadTest.py
@@ -5,11 +5,18 @@ import wx
 rootdir = 'Download_Video'
 list = os.listdir(rootdir)
 filelist = []
+
+menuBar = None
 for i in range(0, len(list)):
     path = os.path.join(rootdir, list[i])
     if not os.path.isfile(path):
-        filelist.append(i)
+        filelist.append(list[i])
 
+#关于这个方法的self 应该是会默认传进来一个self是一个
+# CommandEvent对象应该只能得到按钮的id 不知道怎么直接拿出按钮对象
+def print_menuitem_name(self):
+    #调用全局的变量menuBar
+    print(menuBar.FindItemById(self.Id).Name)
 
 class bucky(wx.Frame):
 
@@ -18,17 +25,16 @@ class bucky(wx.Frame):
                           style=wx.CAPTION | wx.MINIMIZE_BOX | wx.CLOSE_BOX | wx.SYSTEM_MENU)
         panel = wx.Panel(self)
 
-        menubar = wx.MenuBar()
+        _menubar = wx.MenuBar()
         first = wx.Menu()
         load = wx.Menu()
+        for i in filelist:
+            but_1 = load.Append(-1, i)
+            self.Bind(wx.EVT_MENU, print_menuitem_name, but_1)
         first.Append(-1, '加载', load)
         save = first.Append(-1, '保存')
-        menubar.Append(first, '文件')
-
-        for i in filelist:
-            load.Append(-1, i)
-
-        self.SetMenuBar(menubar)
+        _menubar.Append(first, '文件')
+        self.SetMenuBar(_menubar)
 
 
 if __name__ == '__main__':
@@ -36,4 +42,6 @@ if __name__ == '__main__':
     frame = bucky(parent=None, id=-1)
     frame.Show()
     frame.Center()
+    #将当前的MenuBar放在全局
+    menuBar = frame.MenuBar
     app.MainLoop()
