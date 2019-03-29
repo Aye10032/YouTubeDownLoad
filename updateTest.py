eb566ad810c4d9f81d0eb3abc5524ba1bef67cdd
Aye10032 <aye10032@gmail.com>
2019-03-30 03:04:32 +0800

    从GitHub获取最新版本

---
+++ updateTest.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'https://api.github.com/repos/Aye10032/YouTubeDownLoad/releases/latest'
+
+r = requests.get(url)
+downloadLink = r.json()['assets'][0]['browser_download_url']
+appname = r.json()['assets'][0]['name']
+print(downloadLink)
+
+r = requests.get(downloadLink, stream=True)
+f = open(appname, "wb")
+for chunk in r.iter_content(chunk_size=512):
+    if chunk:
+        f.write(chunk)
+
